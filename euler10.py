0e765b570c1b304997ccf3ae3c8fadbb865c3d2a
lindameh <huang.menglin@dhs.sg>
2016-04-17 16:43:29 +0800

    Create euler10.py

---
+++ euler10.py
@@ -0,0 +1,12 @@
+def prime(n):
+    for j in range(2,int(n**0.5)+1):
+        if n%j==0:
+            return False
+    return True
+
+total=0
+for i in range(2,2000000):
+    if prime(i):
+        total+=i
+
+print(total)
